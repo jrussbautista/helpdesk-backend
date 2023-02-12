87732f0e5c206da32fe7595423ef15c976a83259
James Russel C. Bautista <jrussclay@gmail.com>
2023-02-12 18:18:47 +0800

    Add tests for test create project

--- projects/tests/test_views.py
+++ projects/tests/test_views.py
@@ -1,14 +1,27 @@
-from rest_framework.status import HTTP_401_UNAUTHORIZED
+from authentication.models import User
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_201_CREATED
 from rest_framework.test import APIClient
 import pytest
 
 
 @pytest.mark.django_db
 class TestCreateProject:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_anonymous_user_cannot_create_project(self):
         client = APIClient()
         response = client.post(
             "/projects/",
             {"name": "Test Project", "description": "test project description"},
         )
         assert response.status_code == HTTP_401_UNAUTHORIZED
+
+    def test_logged_in_user_can_create_project(self, django_user_model):
+        client = APIClient()
+        user = django_user_model.objects.create(
+            username="test_username", password="test_password"
+        )
+        client.force_authenticate(user=user)
+        response = client.post(
+            "/projects/",
+            {"name": "Test Project", "description": "test project description"},
+        )
+        assert response.status_code == HTTP_201_CREATED
